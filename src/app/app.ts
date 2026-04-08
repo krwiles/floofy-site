e091e7a3f28504e48fde6d0f18f5a81281171912
krwiles <kurtis.r.wiles@gmail.com>
2026-04-08 17:29:09 -0400

    feat(home): add home component with hero section and styles
    - Created home component with HTML structure for hero section.
    - Added CSS for styling the hero section.
    - Implemented basic unit tests for home component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { Navbar } from "./layout/navbar/navbar";
+import { Footer } from "./layout/footer/footer";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
