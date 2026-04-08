6ed86e226cfd0f8819dfe4cd1e0b7a336d6ead35
krwiles <kurtis.r.wiles@gmail.com>
2026-04-08 18:40:41 -0400

    feat: intro animations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
 export const routes: Routes = [
 	{
 		path: '',
-		loadComponent: () => import('./home/home').then(m => m.Home)
+		component: Home,
 	},
 ];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {}
+export class Home implements OnInit {
+  readonly heroImageLoaded = signal(false);
+
+  ngOnInit(): void {
+    const image = new Image();
+    image.src = '/G8s5y4ZakAA0XN9.jpeg';
+
+    if (image.decode) {
+      image
+        .decode()
+        .then(() => this.heroImageLoaded.set(true))
+        .catch(() => this.heroImageLoaded.set(true));
+      return;
+    }
+
+    image.onload = () => this.heroImageLoaded.set(true);
+    image.onerror = () => this.heroImageLoaded.set(true);
+  }
+}
