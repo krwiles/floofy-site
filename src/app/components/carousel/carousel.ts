5b4bd884019bc751445ba5f54cf6e6e8961a513d
krwiles <kurtis.r.wiles@gmail.com>
2026-04-10 00:53:11 -0400

    feat(carousel): refactor carousel component

--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './carousel.html',
   styleUrl: './carousel.css',
 })
-export class Carousel {}
+export class Carousel {
+  images = [
+    { src: '/G_CQjK1XkAALluE.jpeg', alt: 'Image 1' },
+    { src: '/G-Ewq9pagAA7Z-b.jpeg', alt: 'Image 2' },
+    { src: '/G8oM0YEasAAWxIg.jpeg', alt: 'Image 3' },
+    { src: '/G-lXyxKbQAMHkV_.jpeg', alt: 'Image 4' },
+    { src: '/G8s5y4ZakAA0XN9.jpeg', alt: 'Image 5' },
+  ];
+}
