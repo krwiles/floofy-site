a0d3fecb2985124df918d1abcf865abbbe12b88e
krwiles <kurtis.r.wiles@gmail.com>
2026-04-12 16:39:02 -0400

    feat: enhance gallery component with image display and lightbox functionality

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GalleryImageService } from '../services/gallery-image.service';
+import { GalleryImage } from '../models/gallery-image';
+import { DOCUMENT } from '@angular/common';
+import { I18nService } from '../services/i18n.service';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
 })
-export class Gallery {}
+export class Gallery {
+  galleryImageService = inject(GalleryImageService);
+  selectedImage = signal<GalleryImage | null>(null);
+  showLightBox = signal<boolean>(false);
+  scrollY = 0;
+
+    showImage(image: GalleryImage) {
+      this.selectedImage.set(image);
+      this.showLightBox.set(true);
+
+      // Lock scroll
+      this.scrollY = window.scrollY;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${this.scrollY}px`;
+      document.body.style.width = '100%';
+    }
+    
+    closeImage() {
+      this.showLightBox.set(false);
+      this.selectedImage.set(null);
+
+      // Unlock scroll
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+      window.scrollTo(0, this.scrollY); // Restore scroll position
+    }
+}
