d2b99e4ecda8c926ee320dc62a564ebd82c6a888
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2026-05-20 00:46:19 -0400

    feat: enhance about page

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,13 +1,14 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ParallaxSection } from '../components/parallax-section/parallax-section';
 import { TranslatePipe } from '../pipes/translate.pipe';
+import { NgOptimizedImage } from '@angular/common';
 
 // Declare the Twitter widgets object to avoid TypeScript errors
 declare const twttr: { widgets: { load: () => void } };
 
 @Component({
   selector: 'app-about',
-  imports: [ParallaxSection, TranslatePipe],
+  imports: [ParallaxSection, TranslatePipe, NgOptimizedImage],
   templateUrl: './about.html',
   styleUrl: './about.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
