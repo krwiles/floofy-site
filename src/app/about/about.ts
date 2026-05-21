510582fba9009b35b148fca06c05bb1ab5461548
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2026-05-21 12:12:14 -0400

    feat: update about page content and improve accessibility; adjust image attributes and text descriptions

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -2,13 +2,14 @@ import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angu
 import { ParallaxSection } from '../components/parallax-section/parallax-section';
 import { TranslatePipe } from '../pipes/translate.pipe';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 // Declare the Twitter widgets object to avoid TypeScript errors
 declare const twttr: { widgets: { load: () => void } };
 
 @Component({
   selector: 'app-about',
-  imports: [ParallaxSection, TranslatePipe, NgOptimizedImage],
+  imports: [ParallaxSection, TranslatePipe, NgOptimizedImage, RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
