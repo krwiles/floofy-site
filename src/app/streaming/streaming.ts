5b7105be20372b5b938d43f19732504b37cfe769
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2026-06-11 20:06:02 -0400

    feat: update styling and content

--- src/app/streaming/streaming.ts
+++ src/app/streaming/streaming.ts
@@ -9,6 +9,9 @@ import { TranslatePipe } from '../pipes/translate.pipe';
   templateUrl: './streaming.html',
   styleUrl: './streaming.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(window:resize)': 'onWindowResize()',
+  },
 })
 export class Streaming implements AfterViewInit {
   private document = inject(DOCUMENT);
@@ -18,12 +21,33 @@ export class Streaming implements AfterViewInit {
     const script = this.document.createElement('script');
     script.src = 'https://embed.twitch.tv/embed/v1.js';
     script.onload = () => {
-      new (window as any).Twitch.Embed('twitch-embed', {
-        width: 854,
-        height: 480,
-        channel: 'summerfloofy',
-      });
+      this.loadEmbed();
     };
     this.document.body.appendChild(script);
+  }
+
+  loadEmbed() {
+    const embedHost = this.document.getElementById('twitch-embed');
+    if (embedHost) {
+      embedHost.innerHTML = '';
+    }
+
+    return new (window as any).Twitch.Embed('twitch-embed', {
+      width: this.calculateWidth(),
+      height: this.calculateHeight(),
+      channel: 'summerfloofy',
+    });
+  }
+
+  onWindowResize() {
+    this.loadEmbed();
+  }
+
+  calculateHeight() {
+    return Math.round(window.innerHeight * 0.8);
+  }
+
+  calculateWidth() {
+    return Math.min(window.innerWidth, 1280);
   }
 }
