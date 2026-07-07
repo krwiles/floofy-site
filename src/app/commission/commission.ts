5c56cfcfbd9e9c2a684d8be042073b998a3761d5
krwiles <kurtis.r.wiles@gmail.com>
2026-07-07 11:24:25 -0400

    feat(commission): adding form functionality

--- src/app/commission/commission.ts
+++ src/app/commission/commission.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, OnInit } from '@angular/core';
 import { ParallaxSection } from '../components/parallax-section/parallax-section';
 import { TranslatePipe } from '../pipes/translate.pipe';
 import { Carousel } from '../components/carousel/carousel';
 import { GalleryImageService } from '../services/gallery-image.service';
-import { form, FormField } from '@angular/forms/signals';
+import { email, form, FormField, FormRoot, maxLength, required } from '@angular/forms/signals';
 import { PricingService } from '../services/pricing.service';
 import { CurrencyPipe } from '@angular/common';
 
@@ -28,14 +28,16 @@ interface CommissionData {
 
 @Component({
   selector: 'app-commission',
-  imports: [Carousel, ParallaxSection, TranslatePipe, FormField, CurrencyPipe],
+  imports: [Carousel, ParallaxSection, TranslatePipe, FormField, CurrencyPipe, FormRoot],
   templateUrl: './commission.html',
   styleUrl: './commission.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Commission {
+export class Commission implements OnInit {
   private readonly galleryImageService = inject(GalleryImageService);
   readonly pricingService = inject(PricingService);
+  readonly status = signal<string>('');
+  statusElement: HTMLElement | null = null;
 
   private readonly scrollOffset = 108;
   private readonly scrollFocusClass = 'scroll-focus-highlight';
@@ -65,7 +67,36 @@ export class Commission {
     tosAccepted: false,
   });
 
-  commissionForm = form(this.commissionModel);
+  commissionForm = form(this.commissionModel,
+    (schemaPath) => {
+      required(schemaPath.name, { message: 'Name is required.' });
+      required(schemaPath.email, { message: 'Email is required.' });
+      required(schemaPath.description, { message: 'Description is required.' });
+      required(schemaPath.usageType, { message: 'Usage type is required.' });
+      required(schemaPath.tosAccepted, { message: 'You must accept the terms of service to submit the form.' });
+
+      maxLength(schemaPath.email, 100, { message: 'Email cannot exceed 100 characters.' });
+      maxLength(schemaPath.description, 2000, { message: 'Description cannot exceed 2000 characters.' });
+      maxLength(schemaPath.referenceLinks, 1000, { message: 'Reference links cannot exceed 1000 characters.' });
+      maxLength(schemaPath.additionalNotes, 2000, { message: 'Additional notes cannot exceed 2000 characters.' });
+      email(schemaPath.email, { message: 'Please enter a valid email address.' });
+    },
+    {
+      submission: {
+        action: async () => {
+          this.status.set(
+            'Form submitted! Thank you for your commission request. I will review the details and get back to you as soon as possible.',
+          );
+
+          console.log('Commission form submitted:', this.commissionModel());
+        }
+      }
+    }
+  );
+
+  ngOnInit(): void {
+    this.statusElement = document.getElementById('commission-form-status');
+  }
 
   scrollToCommissionTypes(): void {
     this.scrollToElement('commission-types');
